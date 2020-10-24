a0baca0012c36b91aa856711afd7f7d3adfea177
khchoi0301 <khchoi0301@gmail.com>
2020-10-24 21:34:09 +0900

    modify evn params

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
+console.log("env", process.env.MYSQL_ROOT_PASSWORD)
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT,
 });
